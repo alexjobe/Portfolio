9c247f5c78462fbff66c27f0a150e8e712f0034d
Alex Jobe <alex@alexjobe.net>
2019-02-22 19:51:48 -0600

    Fix navbar so it is not visible when the page loads

--- portfolio.js
+++ portfolio.js
@@ -1,38 +1,42 @@
 var navBack = document.getElementById("navbar-background");
+var navProjects = document.getElementById("nav-projects");
+var navContact = document.getElementById("nav-contact");
+var navTitle = document.getElementById("nav-title");
 
-    var navAbout = document.getElementById("nav-about");
-    var navProjects = document.getElementById("nav-projects");
-    var navContact = document.getElementById("nav-contact");
-    var navTitle = document.getElementById("nav-title");
+var about = document.getElementById("about");
+var projects = document.getElementById("projects");
+var contact = document.getElementById("contact");
+var hero = document.getElementById("hero");
 
-    var about = document.getElementById("about");
-    var projects = document.getElementById("projects");
-    var contact = document.getElementById("contact");
-    var hero = document.getElementById("hero");
+document.addEventListener("scroll", function(){
 
-    document.addEventListener("scroll", function(){
-        if(document.documentElement.scrollTop > 600){
-            navBack.style.opacity = 100;
-            navTitle.style.opacity = 100;
-        }
-        else {
-            navBack.style.opacity = 0;
-            navTitle.style.opacity = 0;
-        }
-    });
+    if(document.documentElement.scrollTop > 600){
+
+        navBack.style.transition = "opacity 1s";
+        navTitle.style.transition = "opacity 1s";
+
+        navBack.style.opacity = 100;
+        navTitle.style.opacity = 100;
+    }
+    else {
+        navBack.style.opacity = 0;
+        navTitle.style.opacity = 0;
+    }
+});
+
+navTitle.addEventListener("click", function(){
+    hero.scrollIntoView({behavior: "smooth"});
+});
+
+navAbout.addEventListener("click", function(){
+    about.scrollIntoView({behavior: "smooth"});
+});
+
+navProjects.addEventListener("click", function(){
+    projects.scrollIntoView({behavior: "smooth"});
+});
+
+navContact.addEventListener("click", function(){
+    contact.scrollIntoView({behavior: "smooth"});
+});var navAbout = document.getElementById("nav-about");
     
-    navTitle.addEventListener("click", function(){
-        hero.scrollIntoView({behavior: "smooth"});
-    });
-
-    navAbout.addEventListener("click", function(){
-        about.scrollIntoView({behavior: "smooth"});
-    });
-
-    navProjects.addEventListener("click", function(){
-        projects.scrollIntoView({behavior: "smooth"});
-    });
-
-    navContact.addEventListener("click", function(){
-        contact.scrollIntoView({behavior: "smooth"});
-    });
