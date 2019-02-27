54dd67c098e1964964564d83042e669bd45811d9
Alex Jobe <alex@alexjobe.net>
2019-02-27 17:46:05 -0600

    Tweak JS styling

--- portfolio.js
+++ portfolio.js
@@ -1,45 +1,42 @@
 
-window.onload=function(){
-    var navBack = document.getElementById("navbar-background");
-    var navProjects = document.getElementById("nav-projects");
-    var navContact = document.getElementById("nav-contact");
-    var navTitle = document.getElementById("nav-title");
-    var navAbout= document.getElementById("nav-about");
+window.onload = function () {
+  const navBack = document.getElementById('navbar-background');
+  const navProjects = document.getElementById('nav-projects');
+  const navContact = document.getElementById('nav-contact');
+  const navTitle = document.getElementById('nav-title');
+  const navAbout = document.getElementById('nav-about');
 
-    var about = document.getElementById("about");
-    var projects = document.getElementById("projects");
-    var contact = document.getElementById("contact");
-    var hero = document.getElementById("hero");
+  const about = document.getElementById('about');
+  const projects = document.getElementById('projects');
+  const contact = document.getElementById('contact');
+  const hero = document.getElementById('hero');
 
-    document.addEventListener("scroll", function(){
+  document.addEventListener('scroll', () => {
+    navBack.style.transition = 'opacity 1s';
+    navTitle.style.transition = 'opacity 1s';
 
-        navBack.style.transition = "opacity 1s";
-        navTitle.style.transition = "opacity 1s";
+    if (document.documentElement.scrollTop > 600) {
+      navBack.style.opacity = 100;
+      navTitle.style.opacity = 100;
+    } else {
+      navBack.style.opacity = 0;
+      navTitle.style.opacity = 0;
+    }
+  });
 
-        if(document.documentElement.scrollTop > 600){
+  navTitle.addEventListener('click', () => {
+    hero.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
 
-            navBack.style.opacity = 100;
-            navTitle.style.opacity = 100;
-        }
-        else {
-            navBack.style.opacity = 0;
-            navTitle.style.opacity = 0;
-        }
-    });
+  navAbout.addEventListener('click', () => {
+    about.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
 
-    navTitle.addEventListener("click", function(){
-        hero.scrollIntoView({behavior: "smooth"});
-    });
+  navProjects.addEventListener('click', () => {
+    projects.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
 
-    navAbout.addEventListener("click", function(){
-        about.scrollIntoView({behavior: "smooth"});
-    });
-
-    navProjects.addEventListener("click", function(){
-        projects.scrollIntoView({behavior: "smooth"});
-    });
-
-    navContact.addEventListener("click", function(){
-        contact.scrollIntoView({behavior: "smooth"});
-    });var navAbout = document.getElementById("nav-about");
-}
+  navContact.addEventListener('click', () => {
+    contact.scrollIntoView({ behavior: 'smooth', block: 'start' });
+  });
+};
